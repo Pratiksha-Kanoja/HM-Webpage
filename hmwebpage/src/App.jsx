d75094e83d5f4ebfd34bc5c889b86b60bba8b7f5
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-23 01:30:23 +0530

    Update App.jsx

--- hmwebpage/src/App.jsx
+++ hmwebpage/src/App.jsx
@@ -1,15 +1,24 @@
 
-import './App.css';
+// import './App.css';
 import { Route,Routes } from 'react-router-dom';
-import Homepage from './Components/Homepage';
+import Homepage from './Homepage';
+// import Header from './Components/Header';
+// import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
+    //   <Routes>
+    //     <Route path='/' element={<Homepage/>} />
+    //     <Route path='/header' element={<Header/>} />
+    //   </Routes>
+    // </div>
+    <div>
       <Routes>
-        <Route path='/' element={<Homepage/>} />
+        <Route path='/' element={<Homepage />} />
       </Routes>
     </div>
+
   );
 }
 
