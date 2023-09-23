4951b27efa078333376646e1a393a9361989fc66
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-23 19:46:44 +0530

    Create Homepage.jsx

---
+++ hmwebpage/src/Mainpages/Homepage.jsx
@@ -0,0 +1,13 @@
+// import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import './Homepage.css'
+function Homepage() {
+    return (
+        <div>
+            {/* <Header />  */}
+            {/*this is header Component which we access here */}
+            <Footer />
+        </div>
+    )
+}
+export default Homepage;
