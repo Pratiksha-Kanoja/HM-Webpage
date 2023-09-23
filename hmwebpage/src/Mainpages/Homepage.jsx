02c28f7894a2fad375ff731e2ee406c0d9710495
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-23 23:46:26 +0530

    Update Homepage.jsx

--- hmwebpage/src/Mainpages/Homepage.jsx
+++ hmwebpage/src/Mainpages/Homepage.jsx
@@ -1,10 +1,11 @@
-// import Header from "../Components/Header";
+import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+
 import './Homepage.css'
 function Homepage() {
     return (
         <div>
-            {/* <Header />  */}
+            <Header /> 
             {/*this is header Component which we access here */}
             <Footer />
         </div>
