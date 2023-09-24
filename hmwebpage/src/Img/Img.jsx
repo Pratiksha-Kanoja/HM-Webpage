b97ded5473b9004552a0cb7ed46320db9739f613
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-25 04:16:42 +0530

    Create Img.jsx

---
+++ hmwebpage/src/Img/Img.jsx
@@ -0,0 +1,10 @@
+
+
+const Images ={
+    first: require('./first.jpg'),
+    second: require('./second.jpg'),
+    third: require('./third.jpg'),
+    forth: require('./forth.jpg')
+}
+
+export default Images;
