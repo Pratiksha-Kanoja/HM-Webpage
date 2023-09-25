8b37ae5a7ad4e7a6f12e11b4655532482e20af59
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-25 23:09:04 +0530

    Update Img.jsx

--- hmwebpage/src/Img/Img.jsx
+++ hmwebpage/src/Img/Img.jsx
@@ -4,7 +4,9 @@ const Images ={
     first: require('./first.jpg'),
     second: require('./second.jpg'),
     third: require('./third.jpg'),
-    forth: require('./forth.jpg')
+    forth: require('./forth.jpg'),
+    fifth: require('./fifth.jpg'),
+    six: require('./six.jpg')
 }
 
 export default Images;
