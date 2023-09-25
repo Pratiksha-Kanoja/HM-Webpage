3d35023c163fb22c190b5f7a8169b96c847bb04b
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-26 02:32:46 +0530

    Update App.jsx

--- hmwebpage/src/App.jsx
+++ hmwebpage/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route,Routes } from 'react-router-dom';
 import Homepage from './Mainpages/Homepage';
 import Men from './Mainpages/Men';
-
+// import Register from './Components/Register';
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/Men' element={<Men />} />
+        {/* <Route path='/' element={<Register />} /> */}
       </Routes>
     </div>
 
