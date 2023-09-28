06ac0835f02e4ea257abdb446054dbb9128a0f32
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-29 01:55:14 +0530

    Update App.jsx

--- hmwebpage/src/App.jsx
+++ hmwebpage/src/App.jsx
@@ -3,17 +3,20 @@ import Homepage from './Mainpages/Homepage';
 import Men from './Mainpages/Men';
 import Register from './Components/Register';
 import Login from './Components/Login';
-// import Header from './Components/Header';
+import Header from './Components/Header';
 
+import { useState } from 'react';
 function App() {
+  const [active, setActive] = useState(false);
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage active={active} setActive={setActive}/>} />
         <Route path='/Men' element={<Men />} />
+        <Route path='/Header' element={<Header />} />
         <Route path='/Register' element={<Register />} />
         <Route path='/Login' element={<Login />} />
-        {/* <Route path='/' element={<Header />} /> */}
+       
       </Routes>
     </div>
 
