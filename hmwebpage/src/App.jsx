b9e84a70c1bc436090c03e1716501b449ed0bc2b
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-02 01:30:36 +0530

    Update App.jsx

--- hmwebpage/src/App.jsx
+++ hmwebpage/src/App.jsx
@@ -1,22 +1,17 @@
 import { Route,Routes } from 'react-router-dom';
-import Homepage from './Mainpages/Homepage';
-import Men from './Mainpages/Men';
-import Register from './Components/Register';
-import Login from './Components/Login';
-import Header from './Components/Header';
+// import Homepage from './Mainpages/Homepage';
+// import Men from './Mainpages/Men';
+// import { useState } from 'react';
+import Singleproduct from './Mainpages/Singleproduct';
 
-import { useState } from 'react';
 function App() {
-  const [active, setActive] = useState(false);
+  // const [active, setActive] = useState(false);
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Homepage active={active} setActive={setActive}/>} />
-        <Route path='/Men' element={<Men />} />
-        <Route path='/Header' element={<Header />} />
-        <Route path='/Register' element={<Register />} />
-        <Route path='/Login' element={<Login />} />
-       
+        {/* <Route path='/' element={<Homepage active={active} setActive={setActive}/>} />
+        <Route path='/Men' element={<Men />} /> */}
+        <Route path='/' element={<Singleproduct/>} />
       </Routes>
     </div>
 
