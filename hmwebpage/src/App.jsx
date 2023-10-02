b1085b051bc4ecf1702d8c627f8fccf34848dc66
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-02 22:03:10 +0530

    Update App.jsx

--- hmwebpage/src/App.jsx
+++ hmwebpage/src/App.jsx
@@ -1,17 +1,17 @@
 import { Route,Routes } from 'react-router-dom';
 // import Homepage from './Mainpages/Homepage';
 // import Men from './Mainpages/Men';
-// import { useState } from 'react';
-import Singleproduct from './Mainpages/Singleproduct';
+// import Singleproduct from './Mainpages/Singleproduct';
+import Shoppingbag from './Mainpages/Shoppingbag';
 
 function App() {
-  // const [active, setActive] = useState(false);
   return (
     <div>
       <Routes>
-        {/* <Route path='/' element={<Homepage active={active} setActive={setActive}/>} />
+        {/* <Route path='/' element={<Homepage/>} />
         <Route path='/Men' element={<Men />} /> */}
-        <Route path='/' element={<Singleproduct/>} />
+        {/* <Route path='/' element={<Singleproduct/>} /> */}
+        <Route path='/' element={<Shoppingbag/>} />
       </Routes>
     </div>
 
