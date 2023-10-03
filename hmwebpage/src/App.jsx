98f6c4dbe6ce2eaeb1aed3d1fc1dbd1dcd538631
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-04 02:30:58 +0530

    Update App.jsx

--- hmwebpage/src/App.jsx
+++ hmwebpage/src/App.jsx
@@ -1,9 +1,9 @@
-import { Route,Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Homepage from './Mainpages/Homepage';
 import Men from './Mainpages/Men';
 import Shoppingbag from './Mainpages/Shoppingbag';
 import Singleproduct from './Mainpages/Singleproduct';
-
+// import Heart_Icon from './Components/Heart_Icon';
 function App() {
   return (
     <div>
@@ -12,6 +12,7 @@ function App() {
         <Route path='/Men' element={<Men />} />
         <Route path='/cart' element={<Shoppingbag />} />
         <Route path='/cart/Productpage.1021953016' element={<Singleproduct />} />
+        {/* <Route exact path='/' element={<Heart_Icon/>} /> */}
       </Routes>
     </div>
 
