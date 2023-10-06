8b073611c0de3cc2a08fa3f1b0c486acfd9ca071
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-06 19:20:27 +0530

    Create Extrahome.jsx

---
+++ hmwebpage/src/Extra-component/Extrahome.jsx
@@ -0,0 +1,12 @@
+import Parent from "./Parent"
+import Parent2 from "./Parent2"
+
+function Extrahome(){
+    return(
+        <div>
+            <Parent/>
+            <Parent2/>
+        </div>
+    )
+}
+export default Extrahome
