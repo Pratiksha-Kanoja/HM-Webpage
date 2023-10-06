1d033916d9ba9a28310f7a627658c7cdf2f936e9
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-06 19:20:32 +0530

    Create Parent2.jsx

---
+++ hmwebpage/src/Extra-component/Parent2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Child from './Child';
+ 
+function Parent2(){
+    return (
+        <div>
+            <Child>https://media.istockphoto.com/id/1173733640/photo/green-forest.webp?b=1&s=170667a&w=0&k=20&c=X6WIre2lWdgZeYjYQZdiinaT3yT3PgFr5dEHd9Eq9rE=</Child>
+            <Child>https://media.istockphoto.com/id/184342766/photo/morning-in-the-field.webp?b=1&s=170667a&w=0&k=20&c=DOTZnGrWumQBfc2xBlTOL2oku96cgSZ8pBsDP8qM1WA=</Child>
+            {/* <Child data={data} />
+            <Child data={data} /> */}
+        </div>
+    );
+}
+ 
+export default Parent2;
