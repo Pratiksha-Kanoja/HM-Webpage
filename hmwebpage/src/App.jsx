e3118e7aecfaa5366f370c2fb886db73391d5f98
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-10-12 09:11:11 +0530

    Update App.jsx

--- hmwebpage/src/App.jsx
+++ hmwebpage/src/App.jsx
@@ -1,20 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-// import Homepage from './Mainpages/Homepage';
-// import Men from './Mainpages/Men';
-// import Shoppingbag from './Mainpages/Shoppingbag';
-import Extrahome from './Extra-component/Extrahome';
+import Homepage from './Mainpages/Homepage';
+import Men from './Mainpages/Men';
+import Shoppingbag from './Mainpages/Shoppingbag';
+// import Extrahome from './Extra-component/Extrahome';
 
-// import Singleproduct from './Mainpages/Singleproduct';
+import Singleproduct from './Mainpages/Singleproduct';
 function App() {
   return (
     <div>
       <Routes>
-
-        {/* <Route path='/' element={<Homepage/>} />
+        <Route path='/' element={<Homepage/>} />
         <Route exact path='/Men' element={<Men />} />
         <Route exact path='/cart' element={<Shoppingbag />} />
-        <Route exact path='/cart/Productpage.1021953016' element={<Singleproduct />} /> */}
-        <Route path='/' element={<Extrahome/>} />
+        <Route exact path='/cart/Productpage.1021953016' element={<Singleproduct />} />
+        {/* <Route path='/' element={<Extrahome/>} /> */}
       </Routes>
     </div>
 
